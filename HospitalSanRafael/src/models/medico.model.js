17563aa4fef6763c61095a8cee181e2adcf4fec3
Jhonnatan Ezequiel Peñate Serrano <20245184@esen.edu.sv>
2026-05-19 14:35:30 -0600

    Creacion modelo medico

--- HospitalSanRafael/src/models/medico.model.js
+++ HospitalSanRafael/src/models/medico.model.js
@@ -1,2 +1,38 @@
-// Modelo de Médico
-// id, nombre, especialidad, departamento, horario de atención, estado (activo/guardia/fuera de servicio)
+const medicos = [
+    {
+        id: 1,
+        nombre: "Dr. Alejandro Durón",
+        especialidad: "Cardiología",
+        departamento: "Cardiovasculares",
+        horarioAtencion: [
+            { dia: "Lunes", horaInicio: "08:00", horaFin: "12:00" },
+            { dia: "Miércoles", horaInicio: "08:00", horaFin: "12:00" }
+        ],
+        estado: "activo" 
+    },
+    {
+        id: 2,
+        nombre: "Dra. Reina Sosa",
+        especialidad: "Pediatría",
+        departamento: "Infantil",
+        horarioAtencion: [
+            { dia: "Martes", horaInicio: "13:00", horaFin: "17:00" },
+            { dia: "Jueves", horaInicio: "13:00", horaFin: "17:00" }
+        ],
+        estado: "guardia"
+    },
+    {
+        id: 3,
+        nombre: "Dr. Jhonnatan Peñate",
+        especialidad: "Neurología",
+        departamento: "Neurología",
+        horarioAtencion: [
+            { dia: "Viernes", horaInicio: "09:00", horaFin: "15:00" }
+        ],
+        estado: "fuera de servicio"
+    }
+];
+
+module.exports = {
+    medicos
+};
